78d05387123abf157d3da44dd62e71f212bba357
Jonatan Ebert <jonatan.ebert@gmail.com>
2023-02-28 21:19:05 -0300

    First entity created (City) with StateCode Enum

---
+++ src/main/java/com/eberts/shop/models/City.java
@@ -0,0 +1,60 @@
+package com.eberts.shop.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.eberts.shop.models.enums.StateCode;
+
+public class City implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer ibgeCode;
+	private String name;
+	private String state;
+
+	public City() {
+	}
+
+	public City(String name, String stateAcronym, Integer ibgeCode) {
+		this.name = name;
+		this.ibgeCode = ibgeCode;
+		this.state = StateCode.fromAcronym(stateAcronym).getAcronym();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Integer getIbgeCode() {
+		return ibgeCode;
+	}
+
+	public void setIbgeCode(Integer ibgeCode) {
+		this.ibgeCode = ibgeCode;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		City other = (City) obj;
+		return Objects.equals(ibgeCode, other.ibgeCode);
+	}
+}
---
+++ src/main/java/com/eberts/shop/models/enums/StateCode.java
@@ -0,0 +1,100 @@
+package com.eberts.shop.models.enums;
+
+public enum StateCode {
+	AM("Amazonas", "AM", "Manaus", 13), 
+	AL("Alagoas", "AL", "Maceió", 27), 
+	AC("Acre", "AC", "Rio Branco", 12),
+	AP("Amapá", "AP", "Macapá", 16), 
+	BA("Bahia", "BA", "Salvador", 19), 
+	PA("Pará", "PA", "Belém", 15),
+	MT("Mato Grosso", "MT", "Cuiabá", 51), 
+	MG("Minas Gerais", "MG", "Belo Horizonte", 31),
+	MS("Mato Grosso do Sul", "MS", "Campo Grande", 50), 
+	GO("Goiás", "GO", "Goiânia", 52),
+	MA("Maranhão", "MA", "São Luís", 21), 
+	RS("Rio Grande do Sul", "RS", "Porto Alegre", 43),
+	TO("Tocantins", "TO", "Palmas", 17), 
+	PI("Piauí", "PI", "Teresina", 22), 
+	SP("São Paulo", "SP", "São Paulo", 35),
+	RO("Rondônia", "RO", "Porto Velho", 11), 
+	RR("Roraima", "RR", "Boa Vista", 14), 
+	PR("Paraná", "PR", "Curitiba", 41),
+	CE("Ceará", "CE", "Fortaleza", 23), 
+	PE("Pernambuco", "PE", "Recife", 26),
+	SC("Santa Catarina", "SC", "Florianópolis", 42), 
+	PB("Paraíba", "PB", "João Pessoa", 25),
+	RN("Rio Grande do Norte", "RN", "Natal", 24), 
+	ES("Espírito Santo", "ES", "Vitória", 32),
+	RJ("Rio de Janeiro", "RJ", "Rio de Janeiro", 33), 
+	SE("Sergipe", "SE", "Aracaju", 28),
+	DF("Distrito Federal", "DF", "Brasília", 53);
+
+	private final String name;
+	private final String acronym;
+	private final String capital;
+	private final Integer ibgeCode;
+
+	private StateCode(final String name, final String acronym, final String capital, final Integer ibgeCode) {
+		this.name = name;
+		this.acronym = acronym;
+		this.capital = capital;
+		this.ibgeCode = ibgeCode;
+	}
+
+	public static StateCode fromName(final String nomeUf) {
+		for (final StateCode uf : StateCode.values()) {
+			if (uf.name.equalsIgnoreCase(nomeUf)) {
+				return uf;
+			}
+		}
+
+		throw new IllegalArgumentException(nomeUf);
+	}
+
+	public static StateCode fromAcronym(final String acronym) {
+		for (final StateCode uf : StateCode.values()) {
+			if (uf.acronym.equalsIgnoreCase(acronym)) {
+				return uf;
+			}
+		}
+
+		throw new IllegalArgumentException(acronym);
+	}
+
+	public static StateCode fromCapital(final String capital) {
+		for (final StateCode uf : StateCode.values()) {
+			if (uf.capital.equalsIgnoreCase(capital)) {
+				return uf;
+			}
+		}
+
+		throw new IllegalArgumentException(capital);
+	}
+
+	public static StateCode fromIbgeCode(final Integer ibgeCode) {
+		for (final StateCode uf : StateCode.values()) {
+			if (uf.ibgeCode == ibgeCode) {
+				return uf;
+			}
+		}
+
+		throw new IllegalArgumentException(ibgeCode.toString());
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAcronym() {
+		return acronym;
+	}
+
+	public String getCapital() {
+		return capital;
+	}
+
+	public Integer getIbgeCode() {
+		return ibgeCode;
+	}
+
+}
