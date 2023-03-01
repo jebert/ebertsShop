4ad2eb96f44183bb529951fa700e76af00c43c7d
Jonatan Ebert <jonatan.ebert@gmail.com>
2023-02-28 21:07:47 -0300

    Project created with dependencies

---
+++ src/test/java/com/eberts/shop/EbertsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eberts.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbertsShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
